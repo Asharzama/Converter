c2afa78e0cc3961ee17630a6e6d6911f09acc925
Asharzama <106080617+Asharzama@users.noreply.github.com>
2022-08-14 16:58:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+function convert(){
+    let input = document.getElementsByClassName("input")[0].value;
+    let output = document.getElementsByClassName('output')[0];
+    let from = document.getElementById("from").value;
+    let to = document.getElementById("to").value;
+    
+    // From list
+    switch (from) {
+        case "decimal":
+            input =  parseInt(input, 10).toString(10);
+            break;
+        case "hexadecimal":
+            input =  parseInt(input, 16).toString(10);
+            break;
+        case "octal":
+            input =  parseInt(input, 8).toString(10);  
+            break;
+        case "binary":
+            input =  parseInt(input, 2).toString(10);
+            break;
+    }
+
+    // To list
+    switch (to) {
+        case "decimal":
+            output.value = Number(input).toString(10);    
+            break;
+        case "hexadecimal":
+            output.value = Number(input).toString(16);
+            break;
+        case "octal":
+            output.value = Number(input).toString(8);
+            break;
+        case "binary":
+            output.value = Number(input).toString(2);
+            break;
+    }
+}
+
+function reset(){
+    let input = document.getElementsByClassName("input")[0];
+    let output = document.getElementsByClassName('output')[0];
+
+    input.value = "";
+    output.value = "";
+}
+
+function swap(){
+    let from = document.getElementById("from").value;
+    let to = document.getElementById("to").value;
+
+    document.getElementById("from").value = to;
+    document.getElementById("to").value = from;
+}
